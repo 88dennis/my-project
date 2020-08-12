34a5067d16d65d11464ab1c5e68508ee0f67d637
Dennis <dennissarmiento808@gmail.com>
2020-08-11 23:18:34 -0400

    up

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,4 @@
-const mongoose = require('mongoose');
-const connection = "mongodb+srv://88dennis:88dennis@cluster0-4repr.mongodb.net/myproject?retryWrites=true&w=majority";
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
+// const mongoose = require('mongoose');
+// mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+//     .then(() => console.log("Database Connected Successfully"))
+//     .catch(err => console.log(err));
